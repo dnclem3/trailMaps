15b635bef48db846857c710a4ab07afe2a8c314d
Dan Clem <daniel.p.clem@gmail.com>
2017-04-03 22:24:50 -0700

    working map with protoype for adding lat and long to js leaflet

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,24 @@
 var uniqueId = [];
 
 //Constructor for new bike trail
-function Trail(trailName, distance) {
+function Trail(trailName, distance, lat, long) {
   this.trailName = trailName;
   this.distance = distance;
+  this.lat = lat;
+  this.long = long;
   this.review = [];
 }
 //Method for adding trail to the form
 Trail.prototype.addToTrailList = function() {
   $("#trail").append('<option value="' + this.trailName + '">' + this.trailName + ' </option>');
 }
+
+//Method for adding map coordinates to js Leaflet
+Trail.prototype.mapCoordinates = function() {
+  var mymap = L.map('mapid').setView([this.lat, this.long], 15);
+  return mymap;
+}
+
 //Constructor with all attributes of trail reviews
 function Review(trail, condition, traffic, friendly, rating, comments) {
   this.uniqueId = uniqueId.length + 1
@@ -22,17 +31,20 @@ function Review(trail, condition, traffic, friendly, rating, comments) {
   this.comments = comments;
   this.pushUniqueId(this.uniqueId);
 }
+
 //Method to push objects into variable
 Review.prototype.pushUniqueId = function(id) {
   uniqueId.push(id);
 }
+
 //Create new objects
-var burkeGilman = new Trail("Burke Gilman", 35);
-var elliotBay = new Trail("Elliot Bay", 7);
+var burkeGilman = new Trail("Burke Gilman", 35, 47.593968, -122.306171);
+var elliotBay = new Trail("Elliot Bay", 7, 47.785089, -122.325275);
 
 //Calling method on objects
 burkeGilman.addToTrailList();
 elliotBay.addToTrailList();
+var mymap = elliotBay.mapCoordinates();
 
 //Method to gather inputs from the user
 var getReview = function() {
@@ -44,20 +56,23 @@ var getReview = function() {
   var comments =$("#comments").val();
   var review = new Review(trail, condition, traffic, friendly, rating, comments);
   return review;
-
 }
+
 //Map for biking trails
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZG5jbGVtMyIsImEiOiJjajEyeWxscWIwMGp1MzJwMXByd28waW83In0.PKZV-kndiDaRkLaBz89cvw', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapid',
-    accessToken: 'pk.eyJ1IjoiZG5jbGVtMyIsImEiOiJjajEyeWxscWIwMGp1MzJwMXByd28waW83In0.PKZV-kndiDaRkLaBz89cvw'
-}).addTo(mymap);
-
+var updateMap = function() {     L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZG5jbGVtMyIsImEiOiJjajEyeWxscWIwMGp1MzJwMXByd28waW83In0.PKZV-kndiDaRkLaBz89cvw', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18,
+      accessToken: 'pk.eyJ1IjoiZG5jbGVtMyIsImEiOiJjajEyeWxscWIwMGp1MzJwMXByd28waW83In0.PKZV-kndiDaRkLaBz89cvw'
+  }).addTo(mymap);
+}
+updateMap();
 //Interface Logic
 $(document).ready(function() {
+  $("#trail").on(function() {
+    var mymap = elliotBay.mapCoordinates();
+    updateMap();
+
+  })
   $("form").submit(function(e) {
     e.preventDefault();
     var test = getReview();
